8528a108ac2e29fafc97e22b1b20498685c71f51
Alunos <Alunos@192.168.1.146>
2017-08-12 09:28:26 -0300

    Primeira aula

---
+++ Atividade_1.php
@@ -0,0 +1,8 @@
+<?php
+ $valor = 300;
+ if($valor > 100){
+     echo 'o valor é maior que 100';
+ } else{
+     echo 'o valor é menor que 100';
+ }
+?>
---
+++ Atividade_2.php
@@ -0,0 +1,13 @@
+<?php
+    $valor = -2;
+    if($valor > 0){
+        echo 'valor é maior que zero';
+    } 
+    else if($valor < 0){
+        echo 'valor é menor que zero';
+    } else{
+        echo 'valor é igual a zero';
+    }
+    
+?>
+
